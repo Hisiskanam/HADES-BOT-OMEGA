29a89dc0f9aeb686ab405f3e8afa20bccf6a4b99
Hisiskanam <125500626+Hisiskanam@users.noreply.github.com>
2023-10-12 21:44:57 -0500

    Update config.js

--- config.js
+++ config.js
@@ -8,10 +8,10 @@ import axios from 'axios';
 import moment from 'moment-timezone';
 
 global.owner = [
-  ['5212412377467', 'CREADOR', true],
-  ['5212411233838', 'CREADOR', true],
-  ['5212411719888', 'CREADOR', true],
-  ['5212414147874', 'CREADOR', true]
+  ['51950615082', 'CREADOR', true],
+  ['51950615082', 'CREADOR', true],
+  ['51950615082', 'CREADOR', true],
+  ['51950615082', 'CREADOR', true]
   
 ];
 
